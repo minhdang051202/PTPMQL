4f3290131af1c261d2638a443685416a167ed5d3
minhdang051202 <142491361+minhdang051202@users.noreply.github.com>
2024-03-06 14:54:45 +0700

    my hear

--- MVCmovie/Models/Persion.cs
+++ MVCmovie/Models/Persion.cs
@@ -1,6 +1,13 @@
 namespace MVCmovie.Models;
 public class Person 
 {
-    public string PersonID { get; set;}
+        public Person(string personID, string address) 
+        {
+            this.PersonID = personID;
+    this.Address = address;
+   
+        }
+            public string PersonID { get; set;}
     public string FullName { get; set;}
+    public string Address { get; set;}
 }
