1c72079d9cbcd6416c6a9220c71de93fcd51d6ee
minhdang051202 <142491361+minhdang051202@users.noreply.github.com>
2024-03-06 15:09:46 +0700

    baitapthuchanh

--- MVCmovie/Controllers/HomeController.cs
+++ MVCmovie/Controllers/HomeController.cs
@@ -28,4 +28,11 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    [HttpPost]
+      public IActionResult Index(Person ps)
+    {
+        string strResult = "Xin chao" + ps.PersonID + " - " + ps.FullName + "Nha o " + ps.Address;
+        ViewBag.info = strResult;
+        return View();
+    }
 }
