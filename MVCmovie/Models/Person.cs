59eebeab2e79818ca976595d8ba1f70af2b7b2eb
minhdang051202 <142491361+minhdang051202@users.noreply.github.com>
2024-05-22 13:27:16 +0700

    BTVN

--- MVCmovie/Models/Person.cs
+++ MVCmovie/Models/Person.cs
@@ -1,6 +1,7 @@
 namespace MVCmovie.Models;
 public class Person 
 {
+    [Key]
     public string PersonID { get; set; }
     public string FullName { get; set;}
     public string Address { get; set;}
--- NewApp/obj/Debug/net7.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net7.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c72079d9cbcd6416c6a9220c71de93fcd51d6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
